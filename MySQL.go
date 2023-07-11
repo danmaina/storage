9d8ca6d3825b705a2d424d03d3dc3cfd95e8ecd0
Dan Ndirangu <maina.dan.ndirangu@gmail.com>
2023-07-11 16:55:12 +0300

    Update MySQL.go to include HTTP mysql connection

--- MySQL.go
+++ MySQL.go
@@ -1,10 +1,18 @@
 package storage
 
 import (
+	"errors"
+	"github.com/danmaina/HttpResponse/v2"
+	"github.com/danmaina/logger"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm"
+	"net/http"
 	"time"
+)
+
+const (
+	InternalProcessingError = "an application error occurred while processing"
 )
 
 type Mysql struct {
@@ -38,4 +46,18 @@ func (mysql *Mysql) Connect() (*gorm.DB, error) {
 	con.DB().SetConnMaxLifetime(mysql.MaxLifetime)
 
 	return con, nil
+}
+
+func (mysql *Mysql) ConnectHttp(res http.ResponseWriter) *gorm.DB {
+	logger.INFO("Retrieving MySQL Connection")
+
+	db, errDb := mysql.Connect()
+
+	if errDb != nil {
+		handlers.ReturnResponse(http.StatusInternalServerError, errors.New(InternalProcessingError), nil, res)
+		logger.ERR("Error while getting a connection to Mysql: ", errDb)
+		return nil
+	}
+
+	return db
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/danmaina/storage
 go 1.14
 
 require (
+	github.com/danmaina/HttpResponse/v2 v2.0.0-20230711132514-71d23d8d3479
+	github.com/danmaina/logger v1.0.6
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jinzhu/gorm v1.9.14
-	github.com/mediocregopher/radix/v3 v3.5.2
+	github.com/mediocregopher/radix/v3 v3.8.1
 )
